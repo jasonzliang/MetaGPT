88b0b3f0f20cc8234b69af5b55678b0f1b8c19ee
jason.liang <jason.liang@cognizant.com>
2024-07-09 23:44:08 -0700

    latest

--- experiments/autogen_builder.py
+++ experiments/autogen_builder.py
@@ -265,7 +265,7 @@ def extract_code_from_chat(chat_result):
 def eval_humaneval(
     result_dir="results/humaneval_results_%s" % get_time(space=False),
     # result_dir="results/humaneval_results_2024-06-29_21-35-10",
-    builder_cfg="autogen_crossover.json",
+    builder_cfg="autogen_builder_cfg.json",
     work_dir="groupchat",
     clear_cache=True,
 ):
