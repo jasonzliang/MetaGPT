d9cf055629aee204894a6d46a4af515b1c76fefe
jason.liang <jason.liang@cognizant.com>
2024-08-19 11:38:21 -0700

    latest

---
+++ experiments/results/download.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import sys
+import time
+
+SERVER_IP = "biggpu"
+EXPERIMENT_NAME = "*"
+SERVER_DIR = "~/Desktop/MetaGPT/experiments/results"
+LOCAL_DIR = SERVER_DIR
+
+if __name__ == "__main__":
+    print("Usage: ./download.py [exp_name]")
+    print("Usage: ./download.py [exp_name] [server_ip]")
+    time.sleep(1)
+
+    if len(sys.argv) == 2:
+        EXPERIMENT_NAME = sys.argv[1]
+    elif len(sys.argv) == 3:
+        EXPERIMENT_NAME = sys.argv[1]
+        SERVER_IP = sys.argv[2]
+
+    os.system("rsync -avz %s:%s/%s %s" % \
+        (SERVER_IP, SERVER_DIR, EXPERIMENT_NAME, LOCAL_DIR))
