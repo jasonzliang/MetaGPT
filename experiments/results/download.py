21adffa0f6741ab906342b9ba3054cac24b4d54b
jason.liang <jason.liang@cognizant.com>
2024-08-19 11:46:23 -0700

    latest

--- experiments/results/download.py
+++ experiments/results/download.py
@@ -11,13 +11,16 @@ LOCAL_DIR = SERVER_DIR
 if __name__ == "__main__":
     print("Usage: ./download.py [exp_name]")
     print("Usage: ./download.py [exp_name] [server_ip]")
-    time.sleep(1)
 
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
+        print("Error: not enough arguments!"); exit()
+    elif len(sys.argv) == 2:
         EXPERIMENT_NAME = sys.argv[1]
     elif len(sys.argv) == 3:
         EXPERIMENT_NAME = sys.argv[1]
         SERVER_IP = sys.argv[2]
+    else:
+        print("Error: too many arguments!"); exit()
 
     os.system("rsync -Phavz --stats %s:%s/%s %s" % \
         (SERVER_IP, SERVER_DIR, EXPERIMENT_NAME, LOCAL_DIR))
