9acb1535aa3cf88440ad6ca7ff2b759189fa673c
jason.liang <jason.liang@cognizant.com>
2024-08-30 20:32:09 -0700

    latest

--- experiments/util.py
+++ experiments/util.py
@@ -215,12 +215,15 @@ def extract_evalplus(result_file, logger=None):
         result_dict['hybrid_score'] = \
             0.5 * result_dict["base_score"] + 0.5 * result_dict["plus_score"]
     except:
+        stack_trace = traceback.format_exc()
+        with open(result_file + ".err", "w") as f:
+            f.write(stack_trace)
         if logger is None:
             print("Evalplus extraction failed: %s" % result_file)
-            traceback.print_exc()
+            print(stack_trace)
         else:
             logger.info("Evalplus extraction failed: %s" % result_file)
-            logger.info(traceback.format_exc())
+            logger.info(stack_trace)
     finally:
         return result_dict
 
