0bda9100b99c3b16ca6d5217637fb1eebaf2f8dc
jason.liang <jason.liang@cognizant.com>
2024-11-24 20:54:19 -0800

    fixed

--- experiments/llm_evaluator.py
+++ experiments/llm_evaluator.py
@@ -504,7 +504,7 @@ EVALPLUS_EVAL_CONFIG = {
 SCICODE_EVAL_CONFIG = {
     'n_tries': 1,
     'max_problems': 999,
-    'dataset': 'problems_dev',
+    'dataset': 'problems_all',
     'with_background': False,
     'problem_list': [],
     'debug_mode': 0
