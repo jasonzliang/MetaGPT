f6eb066b848671664fa70386c478bee5623b1edf
jason.liang <jason.liang@cognizant.com>
2024-11-29 18:31:55 -0800

    latest

--- experiments/self_improve.py
+++ experiments/self_improve.py
@@ -285,7 +285,7 @@ Stack trace/exception for test cases:\n%s""" % \
 # -Move all of the update agent prompts to a separate file (Done)
 # -Analyze agent descriptions for solved problems and merge them together (Done)
 # -Visualize stats (num gen to solve problem) as a bar graph (Done)
-# -Compressing agent chat history with LLMLingua (WIP)
+# -Compressing agent chat history with LLMLingua text compressor (Done)
 # -Create a library of different expert agents to choose from (WIP)
 # -Create a hierarchical team where there is a leader that delegates tasks (WIP)
 # -Have multiple rounds of competition between agents, everyone give score to code
