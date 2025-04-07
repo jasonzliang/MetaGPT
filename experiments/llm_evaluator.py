d08ad18b23281193c3099f418e81e549fec97867
jason.liang <jason.liang@cognizant.com>
2025-04-06 20:52:21 -0700

    latest

--- experiments/llm_evaluator.py
+++ experiments/llm_evaluator.py
@@ -239,7 +239,8 @@ class EvalPlusEvaluator(object):
 
     def _sanitize(self, code_dir):
         if not self.sanitize: return
-        ec = os.system("evalplus.sanitize --samples %s >/dev/null 2>&1" % code_dir)
+        os.system("evalplus.sanitize --samples %s >/dev/null 2>&1" % code_dir)
+        os.system("rm -rf %s" % code_dir)
         assert os.path.exists("%s-sanitized" % code_dir)
         assert not os.path.exists(code_dir)
 
